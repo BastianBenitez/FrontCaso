3e9d52f0ecffa3c3f92fd1885f3de093c4e48cab
Bastian Benitez <92552627+BastianBenitez@users.noreply.github.com>
2024-11-09 06:19:24 -0300

    refactor: Refactorizacion del codigo del componente CardMenu

--- src/assets/components/CardMenu.tsx
+++ src/assets/components/CardMenu.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
 
 interface Producto {
   _id: string;
@@ -22,94 +24,84 @@ interface CardMenuProps {
 const CardMenu: React.FC<CardMenuProps> = ({ producto }) => {
   const { nombre, descripcion, precio, imagen, disponible } = producto;
 
+  const cardStyles = {
+    maxWidth: 345,
+    width: "100%",
+    backgroundColor: "rgba(15, 15, 20, 0.85)",
+    color: "white",
+    boxShadow: "0 8px 16px rgba(0, 0, 0, 0.7)",
+    borderRadius: "12px",
+    overflow: "hidden",
+    textAlign: "center",
+    border: "1px solid rgba(255, 255, 255, 0.2)",
+  };
+
+  const imageStyles = {
+    objectFit: "cover",
+    filter: "brightness(0.9)",
+  };
+
+  const contentStyles = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    overflow: "hidden",
+  };
+
+  const textStyles = {
+    fontFamily: "'Poppins', sans-serif",
+    textShadow: "1px 1px 3px rgba(0, 0, 0, 0.5)",
+  };
+
+  const priceStyles = {
+    fontFamily: "'Poppins', sans-serif",
+    fontWeight: "bold",
+    color: "#ff5722",
+    fontSize: "1.2rem",
+    minHeight: "1.5rem", // Asegura que haya espacio suficiente
+  };
+
+  const buttonStyles = {
+    backgroundColor: disponible ? "#ff5722" : "grey",
+    "&:hover": {
+      backgroundColor: disponible ? "#e64a19" : "grey",
+    },
+    fontFamily: "'Poppins', sans-serif",
+    fontSize: "1rem",
+    color: "white",
+    padding: "8px 20px",
+    borderRadius: "8px",
+  };
+
   return (
-    <Card
-      sx={{
-        maxWidth: 345,
-        width: "100%",
-        backgroundColor: "rgba(15, 15, 20, 0.85)",
-        color: "white",
-        boxShadow: "0 8px 16px rgba(0, 0, 0, 0.7)",
-        borderRadius: "12px",
-        overflow: "hidden",
-        textAlign: "center",
-        border: "1px solid rgba(255, 255, 255, 0.2)",
-      }}
-    >
+    <Card sx={cardStyles}>
       <CardMedia
         component="img"
         alt={nombre}
         height="180"
         image={imagen}
-        sx={{
-          objectFit: "cover",
-          filter: "brightness(0.9)",
-        }}
+        sx={imageStyles}
       />
-      <CardContent
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          overflow: "hidden",
-        }}
-      >
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{
-            fontFamily: "'Poppins', sans-serif",
-            fontSize: "1.8rem",
-            textShadow: "1px 1px 3px rgba(0, 0, 0, 0.5)",
-          }}
-        >
+      <CardContent sx={contentStyles}>
+        <Typography gutterBottom variant="h5" component="div" sx={textStyles}>
           {nombre}
         </Typography>
         <Typography
           variant="body2"
           sx={{
+            ...textStyles,
             color: "rgba(255, 255, 255, 0.8)",
-            fontFamily: "'Poppins', sans-serif",
-            textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
             mb: 2,
           }}
         >
           {descripcion}
         </Typography>
-        <Typography
-          variant="body1"
-          sx={{
-            fontFamily: "'Poppins', sans-serif",
-            fontWeight: "bold",
-            color: "#ff5722",
-            fontSize: "1.2rem",
-            minHeight: "1.5rem", // Asegura que haya espacio suficiente
-          }}
-        >
+        <Typography variant="body1" sx={priceStyles}>
           ${precio.toLocaleString()} CLP
         </Typography>
       </CardContent>
-      <CardActions
-        sx={{
-          justifyContent: "center",
-        }}
-      >
-        <Button
-          size="medium"
-          variant="contained"
-          sx={{
-            backgroundColor: disponible ? "#ff5722" : "grey",
-            "&:hover": {
-              backgroundColor: disponible ? "#e64a19" : "grey",
-            },
-            fontFamily: "'Poppins', sans-serif",
-            fontSize: "1rem",
-            color: "white",
-            padding: "8px 20px",
-            borderRadius: "8px",
-          }}
-        >
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Button size="medium" variant="contained" sx={buttonStyles}>
           {disponible ? "Ordenar" : "No disponible"}
         </Button>
       </CardActions>
