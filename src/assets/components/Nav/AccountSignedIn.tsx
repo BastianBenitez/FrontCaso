701f8b132d0fbac309eb48c81d572cf0bead6264
Bastian Benitez <92552627+BastianBenitez@users.noreply.github.com>
2024-11-24 17:26:50 -0300

    fix:

--- src/assets/components/Nav/AccountSignedIn.tsx
+++ src/assets/components/Nav/AccountSignedIn.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Avatar, Button, Menu, MenuItem } from "@mui/material";
 import { useAuth } from "../../../AuthContext";
 import { Link } from "react-router-dom";
-import { AppProvider } from "@toolpad/core/AppProvider";
 
 const AccountSignedIn: React.FC = () => {
   const { user, logout } = useAuth();
@@ -17,72 +16,70 @@ const AccountSignedIn: React.FC = () => {
   };
 
   return (
-    <AppProvider>
-      <Box>
-        {user ? (
-          <>
-            <Avatar
-              alt={user.nombre || "User"}
-              onClick={handleMenuOpen}
-              sx={{ cursor: "pointer" }}
-            />
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleMenuClose}
-            >
-              <MenuItem onClick={handleMenuClose}>
-                <Link
-                  to="/perfil"
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  Perfil
-                </Link>
-              </MenuItem>
-              <MenuItem onClick={handleMenuClose}>
-                <Link
-                  to="/orders"
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  Pedidos
-                </Link>
-              </MenuItem>
-              {(user.isAdmin || user.isOwner) && (
-                <MenuItem onClick={handleMenuClose}>
-                  <Link
-                    to="/admin"
-                    style={{ textDecoration: "none", color: "inherit" }}
-                  >
-                    Administrar
-                  </Link>
-                </MenuItem>
-              )}
-              <MenuItem onClick={logout}>Cerrar Sesión</MenuItem>
-            </Menu>
-          </>
-        ) : (
-          <Button
-            color="inherit"
-            href="/login"
-            sx={{
-              padding: "5px 12px",
-              backgroundColor: "#ff5722", // Mismo color de fondo
-              color: "white",
-              border: "none",
-              borderRadius: "8px",
-              cursor: "pointer",
-              fontSize: "16px",
-              fontWeight: "bold",
-              ":hover": {
-                backgroundColor: "#e64a19", // Un color de hover similar
-              },
-            }}
+    <Box>
+      {user ? (
+        <>
+          <Avatar
+            alt={user.nombre || "User"}
+            onClick={handleMenuOpen}
+            sx={{ cursor: "pointer" }}
+          />
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
           >
-            Iniciar Sesión
-          </Button>
-        )}
-      </Box>
-    </AppProvider>
+            <MenuItem onClick={handleMenuClose}>
+              <Link
+                to="/perfil"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Perfil
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose}>
+              <Link
+                to="/orders"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Pedidos
+              </Link>
+            </MenuItem>
+            {(user.isAdmin || user.isOwner) && (
+              <MenuItem onClick={handleMenuClose}>
+                <Link
+                  to="/admin"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Administrar
+                </Link>
+              </MenuItem>
+            )}
+            <MenuItem onClick={logout}>Cerrar Sesión</MenuItem>
+          </Menu>
+        </>
+      ) : (
+        <Button
+          color="inherit"
+          href="/login"
+          sx={{
+            padding: "5px 12px",
+            backgroundColor: "#ff5722", // Mismo color de fondo
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+            fontSize: "16px",
+            fontWeight: "bold",
+            ":hover": {
+              backgroundColor: "#e64a19", // Un color de hover similar
+            },
+          }}
+        >
+          Iniciar Sesión
+        </Button>
+      )}
+    </Box>
   );
 };
 
